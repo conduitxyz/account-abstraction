29a8554489e8b52e50328ffbdb7bf6aa0414338a
Drew Tada <drew.tada@gmail.com>
2024-08-09 18:28:04 -0600

    deployment update

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 
-import {EntryPoint} from "src/core/EntryPoint.sol";
+import {IEntryPoint} from "src/interfaces/IEntryPoint.sol";
 import {SimpleAccountFactory} from "src/samples/SimpleAccountFactory.sol";
 import {TestCounter} from "src/test/TestCounter.sol";
 import "forge-std/console.sol";
@@ -11,24 +11,25 @@ import "forge-std/console.sol";
 contract Deploy is Script {
     function run()
         external
-        returns (
-            EntryPoint entryPoint,
-            SimpleAccountFactory accountFactory,
-            TestCounter testCounter
-        )
+        returns (SimpleAccountFactory accountFactory, TestCounter testCounter)
     {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
 
         vm.startBroadcast(deployerPrivateKey);
-        console.log("Deploying EntryPoint");
-        entryPoint = new EntryPoint();
 
         console.log("Deploying SimpleAccountFactory");
-        accountFactory = new SimpleAccountFactory(entryPoint);
+        accountFactory = new SimpleAccountFactory{salt: bytes32(0)}(
+            IEntryPoint(0x0000000071727De22E5E9d8BAf0edAc6f37da032)
+        );
 
         console.log("Deploying TestCounter");
-        testCounter = new TestCounter();
+        testCounter = new TestCounter{salt: bytes32(0)}();
 
         vm.stopBroadcast();
+
+        vm.writeFile(
+            "./out/aa",
+            vm.toString(address(0x0000000071727De22E5E9d8BAf0edAc6f37da032))
+        );
     }
 }
